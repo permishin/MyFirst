3e78a10f565373f2d3df3bf8b8809c213397bdb2
permishin <ermishinpavel@gmail.com>
2020-06-21 13:56:09 +0300

    test commit

--- src/main/java/org/example/ServingWebContentApplication.java
+++ src/main/java/org/example/ServingWebContentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
-
+private String a = "a";
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
