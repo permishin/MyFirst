495ac20eeee28f57db4d5ff98d6407be5a2374ad
Permishin <ermishinpavel@gmail.com>
2020-12-15 23:32:59 +0300

    finish commit

--- src/main/java/org/example/controllers/MyFirstController.java
+++ src/main/java/org/example/controllers/MyFirstController.java
@@ -12,60 +12,66 @@ import java.util.Optional;
 
 @Controller
     public class MyFirstController {
-        @Autowired
+    @Autowired
 
-        private MyFirstRepo myFirstRepo;
+    private MyFirstRepo myFirstRepo;
 
-        @GetMapping("/")
-        public String main(Model model) {
-            Iterable<User> posts = myFirstRepo.findAll();
-            model.addAttribute("posts", posts);
-            model.addAttribute("title","Павел Ермишин");
-            return "main";
-        }
-        @PostMapping ("/")
-    public String add(@RequestParam String company, @RequestParam String years, @RequestParam String position, @RequestParam String description, Model model ) {
-           User work = new User(company, years, position, description);
-           myFirstRepo.save(work);
-           Iterable<User> posts = myFirstRepo.findAll();
-           model.addAttribute("posts", posts);
-            return "redirect:/";
-        }
-        @GetMapping ("/{id:\\d+}")
+    @GetMapping("/")
+    public String main(Model model) {
+        Iterable<User> posts = myFirstRepo.findAll();
+        model.addAttribute("posts", posts);
+        model.addAttribute("title", "Павел Ермишин");
+        return "main";
+    }
+
+    @PostMapping("/")
+    public String add(@RequestParam String company, @RequestParam String years, @RequestParam String position, @RequestParam String description, Model model) {
+        User work = new User(company, years, position, description);
+        myFirstRepo.save(work);
+        Iterable<User> posts = myFirstRepo.findAll();
+        model.addAttribute("posts", posts);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id:\\d+}")
     public String details(@PathVariable(value = "id") Long id, Model model) {
-            if (!myFirstRepo.existsById(id)) {
-                return "redirect:/";
-            }
-            Optional<User> user = myFirstRepo.findById(id);
-            ArrayList<User> res = new ArrayList<>();
-            user.ifPresent(res::add);
-            model.addAttribute("user", res);
-            return "details";
-        }
-        @PostMapping ("/{id}/remove")
-    public String delete(@PathVariable(value = "id") Long id, Model model) {
-            User user = myFirstRepo.findById(id).orElseThrow();
-            myFirstRepo.delete(user);
+        if (!myFirstRepo.existsById(id)) {
             return "redirect:/";
         }
-        @GetMapping ("/add")
+        Optional<User> user = myFirstRepo.findById(id);
+        ArrayList<User> res = new ArrayList<>();
+        user.ifPresent(res::add);
+        model.addAttribute("user", res);
+        return "details";
+    }
+
+    @PostMapping("/{id}/remove")
+    public String delete(@PathVariable(value = "id") Long id, Model model) {
+        User user = myFirstRepo.findById(id).orElseThrow();
+        myFirstRepo.delete(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/add")
     public String addpost(Model model) {
-            model.addAttribute("title", "Добавить статью");
-            return "add";
-        }
-        @GetMapping ("/{id}/edit")
+        model.addAttribute("title", "Добавить статью");
+        return "add";
+    }
+
+    @GetMapping("/{id}/edit")
     public String edit(@PathVariable(value = "id") Long id, Model model) {
-            if (!myFirstRepo.existsById(id)) {
-                return "redirect:/details";
-            }
-            Optional<User> user = myFirstRepo.findById(id);
-            ArrayList<User> list = new ArrayList<>();
-            user.ifPresent(list::add);
-            model.addAttribute("user", list);
-            return "edit";
+        if (!myFirstRepo.existsById(id)) {
+            return "redirect:/details";
         }
-        @PostMapping ("/{id}/edit")
-    public String PostEdit(@PathVariable(value = "id") Long id, @RequestParam String company, @RequestParam String years, @RequestParam String position, @RequestParam String description, Model model ) {
+        Optional<User> user = myFirstRepo.findById(id);
+        ArrayList<User> list = new ArrayList<>();
+        user.ifPresent(list::add);
+        model.addAttribute("user", list);
+        return "edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String PostEdit(@PathVariable(value = "id") Long id, @RequestParam String company, @RequestParam String years, @RequestParam String position, @RequestParam String description, Model model) {
         User user = myFirstRepo.findById(id).orElseThrow();
         user.setCompany(company);
         user.setYears(years);
@@ -75,5 +81,10 @@ import java.util.Optional;
         return "redirect:/";
     }
 
+    @GetMapping("/about")
+    public String About(Model model) {
+        model.addAttribute("title", "Павел Ермишин");
+        return "about";
     }
+}
 
